f5e2c517e645304df556315e335d65bc0963c84e
Uchariya <ronak.uchariya@accenture.com>
2023-06-15 19:35:42 +0530

    adding about us and download app section

---
+++ src/app/about-us/about-us.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-us',
+  templateUrl: './about-us.component.html',
+  styleUrls: ['./about-us.component.scss']
+})
+export class AboutUsComponent {
+  btn1='Learn more';
+  btn2='Download app';
+}
